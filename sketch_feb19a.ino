482757243301a65e876ecf1ff54a16ef1c5bfcfc
Dominick Couzijn <37818468+DCouzijn@users.noreply.github.com>
2020-03-05 11:35:01 -0800

    Update sketch_feb19a.ino
    Most up to date changes

--- sketch_feb19a.ino
+++ sketch_feb19a.ino
@@ -15,17 +15,22 @@ int P1Score = 0;
 int P2Score = 0;
 int totalScore = 0;
 int swapCounter = 0;
-int ServeSwap = 0;
-int P1ServeCounter = 0;
-int P2ServeCounter = 2;
+boolean ServeSwap = false;
+int ServeCounter = 1;
+boolean SideSwap = false;
+//has someone won the game yet
+boolean win = false;
+int P1Set = 0;
+int P2Set = 0;
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("P1     ||     P2");
-lcd.setCursor(0,1);
+  lcd.print("P1  S:0||0:S  P2");
+  lcd.setCursor(0,1);
   lcd.print("0      ||     0");
   pinMode(incP1,INPUT);
   pinMode(incP2,INPUT);
@@ -37,62 +42,127 @@ lcd.setCursor(0,1);
 }
 
 void loop() {
-  button1 = digitalRead(incP1);
-  button2 = digitalRead(decP1);
-  button3 = digitalRead(incP2);
-  button4 = digitalRead(decP2);
-  
-  if(P1ServeCounter == 0 && P1ServeCounter < 2 && ServeSwap == 0){
-    P1ServeCounter++;
-    delay(200);
-    lcd.setCursor(0,3);
-    lcd.print("*");
+  //if players are on their original starting postions
+  if(SideSwap == false){
+    button1 = digitalRead(incP1);
+    button2 = digitalRead(decP1);
+    button3 = digitalRead(incP2);
+    button4 = digitalRead(decP2);
+  }
+
+  //if players are swapped from their original starting positions
+  if(SideSwap == true){
+    button1 = digitalRead(incP2); // originally button 3
+    button2 = digitalRead(decP2); // originally button 4
+    button3 = digitalRead(incP1); // originally button 1
+    button4 = digitalRead(decP1); //originally button 2
   }
   
-  if(P2ServeCounter == 0 P2ServeCounter < 2 && ServeSwap == 1){
-    P2ServeCounter++;
-    delay(200);
-    lcd.setCursor(0,14);
-    lcd.print("*");
+  //reset Serve Counter when new set begins or when score is 10-10 or greater
+  if(totalScore == 20 || totalScore == 0){
+    ServeCounter = 1;
+  }
+
+  
+  if(win == false){
+    if(ServeSwap == false){
+      delay(200);
+      digitalWrite(ledP1, HIGH);
+      digitalWrite(ledP2, LOW);
+    } else if(ServeSwap == true){
+      delay(200);
+      digitalWrite(ledP2, HIGH);
+      digitalWrite(ledP1, LOW);
+    }
   }
   
   if(button1 == HIGH){
     P1Score++;
+    ServeCounter++;
     delay(200);
-    lcd.setCursor(1,1);
+    if (SideSwap == false){
+    lcd.setCursor(0,1);
     lcd.print(P1Score);
+    } else {
+      lcd.setCursor(14,1);
+      lcd.print(P1Score);
+    }
   }
 
     if(button3 == HIGH){
     P2Score++;
+    ServeCounter++;
     delay(200);
+    if (SideSwap == false){
     lcd.setCursor(14,1);
     lcd.print(P2Score);
+    } else {
+      lcd.setCursor(0,1);
+      lcd.print(P2Score);
+    }
   }
 
     if(button2 == HIGH){
       if(P1Score > 0){
         P1Score--;
+        ServeCounter--;
       }
-    delay(200);
-    lcd.setCursor(1,1);
-    lcd.print(P1Score);
-  }
+      delay(200);
+      if (SideSwap == false){
+      lcd.setCursor(0,1);
+      lcd.print(P1Score);
+      } else {
+        lcd.setCursor(14,1);
+        lcd.print(P1Score);
+      }
+    }
 
     if(button4 == HIGH){
       if(P2Score > 0){
         P2Score--;
+        ServeCounter--;
       }
-    delay(200);
-    lcd.setCursor(14,1);
-    lcd.print(P2Score);
-
+      delay(200);
+      if (SideSwap == false){
+      lcd.setCursor(14,1);
+      lcd.print(P2Score);
+      } else {
+        lcd.setCursor(0,1);
+        lcd.print(P2Score);
+      }
     }
 
-
-        totalScore = P1Score + P2Score;
+  totalScore = P1Score + P2Score;
 
     if(P1Score >= 11 && (P1Score - P2Score) >= 2){
+      P1Set++;
+      P1Score = 0;
+      P2Score = 0;
+      totalScore = 0;
+      if(SideSwap == false){
+        SideSwap = true; 
+        lcd.setCursor(0,0);
+        lcd.print("P2");
+        lcd.setCursor(14,0);
+        lcd.print("P1");
+        lcd.setCursor(6,0);
+        lcd.print(P2Set);
+        lcd.setCursor(9,0);
+        lcd.print(P1Set);
+      } else if (SideSwap == true){
+        SideSwap = false;
+         lcd.setCursor(0,0);
+        lcd.print("P1");
+        lcd.setCursor(14,0);
+        lcd.print("P2");
+        lcd.setCursor(6,0);
+        lcd.print(P1Set);
+        lcd.setCursor(9,0);
+        lcd.print(P2Set);
+      }
+      lcd.setCursor(0,1);
+      lcd.print("0      ||     0 ");
+      /*
       lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("PLAYER 1 WINS");
@@ -100,7 +170,38 @@ void loop() {
       digitalWrite(ledP1,HIGH);
       delay(500);
       digitalWrite(ledP1,LOW);
+      */
   } else if(P2Score >=11 && (P2Score - P1Score) >= 2){
+    P2Set++;
+    P1Score = 0;
+    P2Score = 0;
+    totalScore = 0;
+    if(SideSwap == false){
+        SideSwap = true;
+        lcd.setCursor(0,0);
+        lcd.print("P2");
+        lcd.setCursor(14,0);
+        lcd.print("P1");
+        lcd.setCursor(6,0);
+        lcd.print(P2Set);
+        lcd.setCursor(9,0);
+        lcd.print(P1Set); 
+      } else if (SideSwap == true){
+        SideSwap = false;
+         lcd.setCursor(0,0);
+        lcd.print("P1");
+        lcd.setCursor(14,0);
+        lcd.print("P2");
+        lcd.setCursor(6,0);
+        lcd.print(P1Set);
+        lcd.setCursor(9,0);
+        lcd.print(P2Set);
+      }
+    
+    lcd.setCursor(0,1);
+    lcd.print("0      ||     0 ");
+    
+    /*
             lcd.clear();
       lcd.setCursor(0,0);
       lcd.print("PLAYER 2 WINS");
@@ -108,23 +209,65 @@ void loop() {
       digitalWrite(ledP2,HIGH);
       delay(500);
       digitalWrite(ledP2,LOW);
+      */
   }
+
+  //when P1 wins
+  if(P1Set == 3){
+      win = true;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("PLAYER 1 WINS!!!");
+      if(SideSwap == false){
+        digitalWrite(ledP2,LOW); 
+        delay(500);
+        digitalWrite(ledP1,HIGH);
+        delay(500);
+        digitalWrite(ledP1,LOW);
+      } else if (SideSwap == true){
+        digitalWrite(ledP1, LOW);
+        delay(500);
+        digitalWrite(ledP2,HIGH);
+        delay(500);
+        digitalWrite(ledP2,LOW);
+      }
+  }else if (P2Set == 3){
+      win = true;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("PLAYER 2 WINS!!!");
+      if(SideSwap == false){
+        digitalWrite(ledP1, LOW);
+        delay(500);
+        digitalWrite(ledP2,HIGH);
+        delay(500);
+        digitalWrite(ledP2,LOW);
+      } else if (SideSwap == true){
+        digitalWrite(ledP2,LOW); 
+        delay(500);
+        digitalWrite(ledP1,HIGH);
+        delay(500);
+        digitalWrite(ledP1,LOW);
+      }
+  }
+  //server & receiver position change upon decrement of score changes go below
   
+  //if score is not 10-10
   if(totalScore < 20){
-    if(P1ServeCounter == 2){
-      ServeSwap = 1;
-      P1ServeCounter == 0;
-    } else if(P2ServeCounter == 2){
-      ServeSwap = 0;
-      P2ServeCounter == 0;
-    } 
+    if(ServeCounter == 3 && ServeSwap == false){
+      ServeSwap = true;
+    } else if(ServeCounter == 5 && ServeSwap == true){
+      ServeSwap = false;
+      ServeCounter = 1;
+    }
+    //if score is 10-10 or greater and someone hasn't won yet
   } else if (totalScore >= 20){
-    if(P1ServeCounter == 1){
-      ServeSwap = 1;
-      P1ServeCounter == 0;
-    } else if (P2ServeCounter == 1){
-      ServeSwap = 0;
-      P2ServeCounter == 0;
+    if(ServeCounter == 2 && ServeSwap == true){
+      ServeSwap = false;
+      ServeCounter = 1;
+    } else if (ServeCounter == 2 && ServeSwap == false){
+      ServeSwap = true;
+      ServeCounter = 1;
     }
   }
   
@@ -143,5 +286,3 @@ void loop() {
   }
   */
 }
-
-
